472e8bc925697437e645d6894de59f5f8ceb00b6
Default user <lm.mercado@uniandes.edu.co>
2019-10-29 21:30:31 +0000

    agregar valor radio

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main(void){
+    
+    
+    float N;
+    std::cout<<"Diga el valor del radio "<<std::endl;
+    std::cin>>N;
+    std::cout<<"valor del radio"<<" "<<N<<std::endl;
+    
+    
+    
+    
+    return 0;
+}
