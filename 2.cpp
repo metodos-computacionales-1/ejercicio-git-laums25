b80c1c7ac3e8753f110a6593a54614529fd74599
laums25 <lm.mercado@uniandes.edu.co>
2019-10-29 23:55:40 +0000

    imprimir los tres valores

--- 2.cpp
+++ 2.cpp
@@ -9,6 +9,8 @@ int main(void){
     std::cout<<"Ingrese los tres valores "<<std::endl;
     std::cin>>N>>A>>B;
     
+    std::cout<<"Tres numeros para un triangulo:"<<" "<<N<<" "<<A<<" "<<B<<std::endl;
+    
     return 0;
     
 }
