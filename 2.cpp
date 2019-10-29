51329878a806dc800054543a0575b6c79a8dc081
laums25 <lm.mercado@uniandes.edu.co>
2019-10-29 23:53:56 +0000

    agregar tres valores

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+int main(void){
+    
+    
+    int N;
+    int A;
+    int B;
+    std::cout<<"Ingrese los tres valores "<<std::endl;
+    std::cin>>N>>A>>B;
+    
+    return 0;
+    
+}
